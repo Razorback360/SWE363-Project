bfd4fafdf13cb61928d09a7b1ac6f87417152165
Abdullah Aslan <abullahaslan3010@gmail.com>
2024-11-04 23:27:15 +0300

    as

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <p>Aslan waz here</p>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
