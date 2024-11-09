cbde7a67855534a23b9f59c167f6a854eb2e2e30
Reda Al Sulais <u.yokozuna@gmail.com>
2024-11-09 17:33:52 +0300

    run prettier and update pnpm-lock.yaml

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import Header from "./components/Header.tsx"
+
 import { FileRouter } from './FileRouter.tsx';
+import Header from './components/Header.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
