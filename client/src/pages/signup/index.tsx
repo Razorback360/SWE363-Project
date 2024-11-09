a39ba95227314aa4c4dd2fb268a916cfaa61277a
KingShaxi <shaxxxilol@gmail.com>
2024-11-09 18:05:18 +0300

    Add: Sign up and Log in

---
+++ client/src/pages/signup/index.tsx
@@ -0,0 +1,39 @@
+import "./signup.css";
+
+const signup = () => {
+  return (
+    <div className="sign-up-page-container">
+      <div className="sign-up-container">
+        <h1>Welcome!</h1>
+        <p>Please sign up to continue</p>
+        <form>
+          <label>Email Address</label>
+          <input type="email" placeholder="Placeholder" />
+
+          <label>Password</label>
+          <input type="password" placeholder="Placeholder" />
+          <p className="password-note">
+            It must be a combination of minimum 8 letters, numbers, and symbols.
+          </p>
+
+          <label>Confirm Password</label>
+          <input type="password" placeholder="Placeholder" />
+
+          <div className="sign-up-checkbox-container">
+            <div className="checkbox-label">
+              <input type="checkbox" />
+              <label>Remember me</label>
+            </div>
+          </div>
+
+          <button type="submit">Sign Up</button>
+          <p>
+            Have an account? <a href="/login">Sign in</a>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default signup;
