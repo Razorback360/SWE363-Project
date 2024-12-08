b997104e98f5d92d852e0a1b117491575c2f1dda
Razorback360 <razorback@movies4discord.tk>
2024-12-08 18:25:22 +0300

    feat: Add mongoose with express, add sample routes, create sample model

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,42 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+import User from './models/user.js';
 
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/notes';
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+// Sample routes, not to be used in deployment, only for reference.
+app.get("/users", async (req, res) => {
+  const allUsers = await User.find();
+  return res.status(200).json(allUsers);
 });
+
+app.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  return res.status(200).json(user);
+});
+
+// Start Block for Server & Database Connection
+const start = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+start();
