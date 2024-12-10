1c5a10cc50c3477b9f9e36ce084b3c268987efb1
Razorback360 <razorback@movies4discord.tk>
2024-12-10 21:53:18 +0300

    fix: use universal-cookie to set path

--- client/src/utils/authAPI.ts
+++ client/src/utils/authAPI.ts
@@ -1,20 +1,23 @@
+import Cookies from 'universal-cookie';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const login = async (authData: any) => {
     try {
-      const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000'; // For Vite
+      const apiUrl = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000'; // For Vite
   
       const response = await fetch(`${apiUrl}/api/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(authData),
       });
-  
+      
       if (!response.ok) {
         console.error(`HTTP error! Status: ${response.status}`);
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-  
+      const cookies = new Cookies();
       const data = await response.json();
+      cookies.set('user', data.user, { path: '/' });
+      cookies.set('isHospital', data.isHospital, { path: '/' });
       return data;
     } catch (error) {
       console.error('Error scheduling appointment:', error);
@@ -25,7 +28,7 @@ export const login = async (authData: any) => {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const register = async (authData: any) => {
     try {
-      const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000'; // For Vite
+      const apiUrl = import.meta.env.VITE_API_URL || 'http://127.0.0.1:5000'; // For Vite
   
       const response = await fetch(`${apiUrl}/api/auth/register`, {
         method: 'POST',
@@ -37,8 +40,10 @@ export const register = async (authData: any) => {
         console.error(`HTTP error! Status: ${response.status}`);
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-  
+      const cookies = new Cookies();
       const data = await response.json();
+      cookies.set('user', data.user, { path: '/' });
+      cookies.set('isHospital', data.isHospital, { path: '/' });
       return data;
     } catch (error) {
       console.error('Error scheduling appointment:', error);
